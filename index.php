8678d12fc366ad911b5bdd11f5b6c9177e31d76d
Guillermo Laseca <guillelaseca@gmail.com>
2020-08-31 13:49:36 +0200

    Step 4: PHP and HTML

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
-<?php
+<!--
+Instrucciones:
+ # Desde el terminal
+  php -S localhost:8888
+ # Desde el navegador
+  http://localhost:8888/?name=will
+-->
 
-$greeting = "Hello";
-$name = "Guillermo Laseca";
-
-echo "{$greeting}, {$name}";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>
+            <?= "Hello, " . htmlspecialchars($_GET['name']); ?>
+        </h1>
+    </header>
+</body>
+</html>
