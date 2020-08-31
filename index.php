ccd2edec47553e4b6f2db5080f44308c81c6173e
Guillermo Laseca <guillelaseca@gmail.com>
2020-08-31 13:57:39 +0200

    Step 5: Separate PHP Logic From Presentation

--- index.php
+++ index.php
@@ -1,29 +1,12 @@
-<!--
+<?php
+/*
 Instrucciones:
  # Desde el terminal
   php -S localhost:8888
  # Desde el navegador
   http://localhost:8888/?name=will
--->
+*/
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>
-            <?= "Hello, " . htmlspecialchars($_GET['name']); ?>
-        </h1>
-    </header>
-</body>
-</html>
+$greeting = 'Hello World';
+
+require 'index.view.php';
