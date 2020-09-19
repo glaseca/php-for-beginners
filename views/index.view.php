522b3b9bd91c451d8b4432254412e687f538e76c
Guillermo Laseca <guillelaseca@gmail.com>
2020-09-20 01:24:29 +0200

    Dry Up Your Views

--- views/index.view.php
+++ views/index.view.php
@@ -1,32 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <nav>
-        <li><a href="about">About Page</a></li>
-        <li><a href="contact-our-company">Contact Page</a></li>
-    </nav>
-    <h1>Task For The Day</h1>
-    <ul>
-        <?php foreach($tasks as $task) : ?>
-            <li>
-                <?php if($task->completed) : ?>
-                    <strike><?= $task->description; ?></strike>
-                <?php else : ?>
-                    <?= $task->description; ?>
-                <?php endif; ?>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-</html>
+<?php require 'partials/head.php'; ?>
+
+<h1>Task For The Day</h1>
+<ul>
+    <?php foreach($tasks as $task) : ?>
+        <li>
+            <?php if($task->completed) : ?>
+                <strike><?= $task->description; ?></strike>
+            <?php else : ?>
+                <?= $task->description; ?>
+            <?php endif; ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
+<?php require 'partials/footer.php'; ?>
---
+++ views/partials/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/about/culture">About Our Culture</a></li>
+        <li><a href="/contact-our-company">Contact Page</a></li>
+    </ul>
+</nav>
